a067210960d346ee787002418ff35a091aca074a
Vaibhavraj Kathare <60044609+Vaibhavraj61@users.noreply.github.com>
2020-11-11 10:34:23 +0530

    Updated

--- Chemistry_Expt_01/js/index.js
+++ Chemistry_Expt_01/js/index.js
@@ -1,78 +1,74 @@
+$(document).ready(function () {
 
-$(document).ready(function(){
-    
-    var incr=1,ic=1;
-    $("#simulation,#step-no1,#step-no2,#step-no3,#step-no4,#step-no5,#step-no6,#step-no7,#step-no8,#step-no9,#step-no10,#btn-next,#btn-stop,#img1,#img2,#img3,#img4,#img5,#img6,#img7,#img8,#img9,#task2,#h6").hide();
-    
+    var incr = 1, ic = 1;
+    $("#simulation,#step-no1,#step-no2,#step-no3,#step-no4,#step-no5,#step-no6,#step-no7,#step-no8,#step-no9,#step-no10,#btn-next,#btn-stop,#btn-finish,#btn-finish,#img1,#img2,#img3,#img4,#img5,#img6,#img7,#img8,#img9,#task2,#h6").hide();
+    $("#step-no1,#step-no2,#step-no3").css("color", "black");
 
-    $("#btn-start").click(function(){
+    $("#btn-start").click(function () {
         $("#simulation,#btn-next,#btn-stop,#title1").show();
-        $('#btn-start,#img6,#img8,#img9').hide();
-        });
-    $("#btn-next").click(function(){
-        /*$("#step-no"+incr+",#img"+incr).show();
+        $('#btn-start,#btn-finish,#img6,#img8,#img9').hide();
+    });
+
+    $("#btn-next").click(function () {
+        $("#step-no" + incr + ",#img" + incr).show();
+        $("#step-no" + incr).css("color","white");
+        $("#step-no" + incr).prev().css("color","black");
+        $("#img" + incr).prev().hide();
+        incr += 1;
         
-        incr+=1;*/
-        //alert(incr);
-        
-        $("#step-no"+incr+",#img"+incr).show();
-        //if(incr==4){$("#step-no"+incr).hide();}
-        
-        $("#img"+incr).prev().hide();
-        incr+=1;
         $('#img4,#img5,#img6,#img8,#img9').hide()
-        if(incr==5)
-        {
+        if (incr == 5) {
             $("#title1,#step-no1,#step-no2,#step-no3,#img1,#img2,#img3").hide();
             $('#step-no4').show();
         }
-        if(incr==6)
-        {
+        if (incr == 6) {
             $('#img4').show();
         }
-        if(incr==7)
-        {
+        if (incr == 7) {
             $('#img5,#img6').show();
             $('#img7,#img8,#img9').hide();
-            
+
         }
-        
-        if(incr==8)
-        {
+
+        if (incr == 8) {
             $('#h6').show();
             $('#img7,#img8,#img9').hide();
         }
-        if(incr==9)
-        {
+        if (incr == 9) {
             $('#img7,#img8').show();
+            $('#step-no9').focus();
             $('#img7,#img8').css("visibility", "visible");
         }
-        if(incr==10)
-        {
+        if (incr == 10) {
             $('#img7,#img8').show();
             $('#img7,#img8').css("visibility", "visible");
             $('#img9').hide();
         }
 
-        if(incr==11)
-        {
+        if (incr == 11) {
             $('#img8').hide();
             $('#img9').show();
             $('#img9').css("visibility", "visible");
         }
 
-        if(incr==12)
-        {
+        if (incr == 12) {
+            $('#img9').show();
+            $('#img9').css("visibility", "visible");
             alert('End of Simulation');
-            $("#simulation,#step-no1,#step-no2,#step-no3,#step-no4,#step-no5,#step-no6,#step-no7,#step-no8,#step-no9,#step-no10,#btn-next,#btn-stop,#img1,#img2,#img3,#img4,#img5,#img6,#img7,#img8,#img9,#h6").hide(); //for hide everything
-            incr=1;
-            $("#btn-start").show();
+            $("#btn-next,#btn-stop").hide();
+            $("#btn-finish").show();
         }
-
     });
-    $("#btn-stop").click(function(){
-        $("#simulation,#step-no1,#step-no2,#step-no3,#step-no4,#step-no5,#step-no6,#step-no7,#step-no8,#step-no9,#step-no10,#btn-next,#btn-stop,#img1,#img2,#img3,#img4,#img5,#img6,#img7,#img8,#img9,#h6").hide(); //for hide everything
-        incr=1;
+
+    $("#btn-stop").click(function () {
+        $("#simulation,#step-no1,#step-no2,#step-no3,#step-no4,#step-no5,#step-no6,#step-no7,#step-no8,#step-no9,#step-no10,#btn-next,#btn-stop,#img1,#img2,#img3,#img4,#img5,#img6,#img7,#img8,#img9,#h6").hide();
+        incr = 1;
+        $("#btn-start").show();
+    });
+
+    $("#btn-finish").click(function () {
+        $("#simulation,#step-no1,#step-no2,#step-no3,#step-no4,#step-no5,#step-no6,#step-no7,#step-no8,#step-no9,#step-no10,#btn-next,#btn-stop,#btn-finish,#img1,#img2,#img3,#img4,#img5,#img6,#img7,#img8,#img9,#h6").hide();
+        incr = 1;
         $("#btn-start").show();
     });
 
